5beb17347fbe56f907fa8e929e85ff3d67d3cbfe
ramazan <ramazantzr@hotmail.com>
2021-06-07 00:03:44 +0300

    -Aciklama satirlari ekleniyor (3/1)

--- lib/User.py
+++ lib/User.py
@@ -16,7 +16,7 @@ class User(UserMixin):
     @staticmethod
     def get_user(_user):
         """
-
+            Bu fonksiyon veritabanından admin kullanıcısını çeker.
         :param _user:
         :return:
         """
@@ -31,11 +31,15 @@ class User(UserMixin):
             return AnonUser()
 
     def get_id(self):
+        """
+            Bu fonksiyon giriş yapan kullanıcının id'sini geri verir.
+        :return:
+        """
         return self.user_id
 
     def login_user(self, username, password):
         """
-
+            Bu fonksiyon admin sayfasına giriş yapmanızı sağlar.
         :param username: string
         :param password: hashed string
         :return: User
@@ -50,27 +54,65 @@ class User(UserMixin):
         return u
 
     def get_contacts(self):
+        """
+            Bu fonksiyon sayfa üzerinden gönderilen mesajları gösterir.
+        :return:
+        """
         return self.db.contact.find()
 
     def get_gallerys(self):
+        """
+            Bu fonksiyon sayfa üzerindeki gallery sayfasındaki resimleri gösterip göstermeyeceğinizi ayarlar.
+        :return:
+        """
         return self.db.gallery.find()
 
     def get_blogs(self):
+        """
+            Bu fonksiyon sayfadaki blog yazılarını çeker.
+        :return:
+        """
         return self.db.blog.find()
 
     def get_blog(self, _id):
+        """
+            Bu fonksiyon sayfadaki blog yazısını düzenlemenizi sağlar.
+        :param _id:
+        :return:
+        """
         return self.db.blog.find_one({'_id': ObjectId(_id)})
 
     def remove_blog(self, _id):
+        """
+            Bu fonksiyon sayfadaki blog yazısını silmenizi sağlar.
+        :param _id:
+        :return:
+        """
         self.db.blog.remove({'_id': ObjectId(_id)})
 
     def update_blog(self, _id, posted_blog):
+        """
+            Bu fonksiyon sayfadaki blog yazısını düzenlemenizi sağlar.
+        :param _id:
+        :param posted_blog:
+        :return:
+        """
         return self.db.blog.update({'_id': ObjectId(_id)}, {'$set': posted_blog})
 
     def new_blog(self, posted_blog):
+        """
+            Bu fonksiyon sayfaya yeni blog yazısı ekler.
+        :param posted_blog:
+        :return:
+        """
         self.db.blog.insert(posted_blog)
 
     def read_contact(self, _id):
+        """
+            Bu fonksiyon sayfadan gelen iletişim yazısının okundu bilgisini günceller.
+        :param _id:
+        :return:
+        """
         self.db.contact.update({'_id': ObjectId(_id)}, {
             '$set': {
                 'is_active': False
@@ -78,6 +120,11 @@ class User(UserMixin):
         })
 
     def image_activity(self, _id):
+        """
+            Bu fonksiyon gallery sayfasındaki resimlerin görününrlüğünü ayarlar.
+        :param _id:
+        :return:
+        """
         image = self.db.gallery.find_one({'_id': ObjectId(_id)})
         self.db.gallery.update({'_id': ObjectId(_id)}, {
             '$set': {
@@ -86,6 +133,11 @@ class User(UserMixin):
         })
 
     def blog_activity(self, _id):
+        """
+            Bu fonksiyon blog sayfasındaki yazıların görününrlüğünü ayarlar.
+        :param _id:
+        :return:
+        """
         blog = self.db.blog.find_one({'_id': ObjectId(_id)})
         self.db.blog.update({'_id': ObjectId(_id)}, {
             '$set': {
